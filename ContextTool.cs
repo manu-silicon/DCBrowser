77078f555686dbb66dd390d7a839f3a242cd4264
Manu <manu-silicon@users.noreply.github.com>
2015-11-05 17:25:05 +0900

    Initial trial to build a new VisualStudio Tool for code browsing.

---
+++ ContextTool.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ContextTool.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace DCBrowser
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("7f0d7af1-28d0-49c3-96de-a9ee7d42cce7")]
+    public class ContextTool : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContextTool"/> class.
+        /// </summary>
+        public ContextTool() : base(null)
+        {
+            this.Caption = "ContextTool";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ContextToolControl();
+        }
+    }
+}
---
+++ ContextToolControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ContextToolControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace DCBrowser
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ContextToolControl.
+    /// </summary>
+    public partial class ContextToolControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContextToolControl"/> class.
+        /// </summary>
+        public ContextToolControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "ContextTool");
+        }
+
+        private void OnClassNameKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void OnMemberNameKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+    }
+}
